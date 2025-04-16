c2a173c4f3f743359153ae1755a9cdae1a5afaaf
user3124 <username040424@gmail.com>
2025-04-16 23:15:47 +0700

    Отменил прошлый коммит

--- hw3/Program.cs
+++ hw3/Program.cs
@@ -10,37 +10,7 @@ namespace hw3
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Введите размер квадратной матрицы:");
-                int matrixSize = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Введите минимальное значение:");
-                int minimumValue = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("Введите максимальное значение:");
-                int maximumValue = Convert.ToInt32(Console.ReadLine());
-
-                SquareMatrix testMatrix = new SquareMatrix(matrixSize, minimumValue, maximumValue);
-
-                Console.WriteLine("\nСгенерированная матрица:");
-                Console.WriteLine(testMatrix.ToString());
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Ошибка: введено нечисловое значение.");
-            }
-            catch (ArgumentException exception)
-            {
-                Console.WriteLine($"Ошибка: {exception.Message}");
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine($"Произошло непредвиденное исключение: {exception.Message}");
-            }
-
-            Console.WriteLine("\nНажмите любую клавишу для выхода...");
-            Console.ReadKey();
+            // Здесь что-то будет
         }
     }
 }
